a6294e862cd175ed19e95b00a1af810a8b40f222
jamesdurler <jdurler@companieshouse.gov.uk>
2025-05-21 09:45:45 +0100

    fact which reads the companies house preference domain if nothing is read Unknown is returned

---
+++ facts/ch_role.py
@@ -0,0 +1,12 @@
+'''Get the role from a managed preferences domain '''
+from CoreFoundation import CFPreferencesCopyAppValue
+
+def fact():
+    role = CFPreferencesCopyAppValue('Role', 'uk.gov.companieshouse.role')
+    if department is not None:
+        return {'ch_role': role}
+    return {'ch_role': 'Unknown'}
+
+if __name__ == '__main__':
+    print(fact())
+
